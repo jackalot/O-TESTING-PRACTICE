60528c7571713e136c501004fff08a08f464f0dc
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-21 09:46:22 -0500

    added some declarations to the function inside reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,8 @@
-function reverseString()
+function reverseString(str)
 {
-
+    const split = str.split();
+    const reverse = split.reverse();
+    const joined = reverse.join();
+    return joined;
 }
 module.exports = reverseString;
