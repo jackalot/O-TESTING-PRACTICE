31644b68ac35bb8eef05c7d5e45abf9fbd42dc03
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-22 09:04:36 -0500

    added tests to ceaserCipher.test.js

--- ceaserCipher.test.js
+++ ceaserCipher.test.js
@@ -1,1 +1,14 @@
 const ceaserCipher = require('./ceaserCipher');
+
+test('The string "we will attack at dawn" returns "xf xjmm buubdl bu ebxo"', () => {
+    expect(ceaserCipher("we will attack at dawn")).toBe("xf xjmm buubdl bu ebxo");
+})
+test('The string "z" returns "a"', () => {
+    expect(ceaserCipher("z")).toBe("a");
+})
+test('The string "Taco" returns "Ubdp"', () => {
+    expect(ceaserCipher("Taco")).toBe("Ubdp");
+})
+test('The string "can`t" returns "dbo`t"', () => {
+    expect(ceaserCipher("can`t")).toBe("dbo`t");
+})
