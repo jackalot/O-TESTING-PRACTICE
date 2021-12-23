3109a27bb474767ff8e99044a96a32b6a957b4f1
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-23 10:18:23 -0500

    changed the test from toBe to toStrictEqual. reason: test will fail with 'serializes to the same string' with toBe

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,5 +1,5 @@
 const analyzeArray = require ("./analyzeArray");
 
 test('Takes in the array "1,8,3,4,2,6" and returns an object = { average: 4, min: 1, max: 8, length: 6}"', () => {
-    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toBe({ average: 4, min: 1, max: 8, length: 6});
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({ average: 4, min: 1, max: 8, length: 6});
 })
