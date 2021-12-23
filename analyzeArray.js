56155f1cb9eb944d4e75bbd4b8f6f40b93d5e8ee
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-23 09:41:21 -0500

    added the psuedo code from the plan to the script, removed an = sign in if statements

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,4 +1,38 @@
-function analyzeArray () {
-
+function getAverage(inputArray) {
+    let tempSum = 0;
+    for(let i = 0; i < inputArray.length; i++)
+    {
+        tempSum += inputArray[i];
+    }
+    return tempSum;
+}
+function getMinimum(inputArray) {
+    let tempMin = 100;
+    for(let i = 0; i < inputArray.length; i++)
+    {
+        if(inputArray[i] <= tempMin)
+        {
+            tempMin = inputArray[i];
+        }
+    }
+    return tempMin;
+}
+function getMaximum(inputArray) {
+    let tempMax = 0;
+    for(let i = 0; i > inputArray.length; i++)
+    {
+        if(inputArray[i] >= tempMax)
+        {
+            tempMax = inputArray[i];
+        }
+    }
+    return tempMax;
+}
+function analyzeArray(inputArray) {
+const average = getAverage(inputArray);
+const min = getMinimum(inputArray);
+const max = getMaximum(inputArray);
+const length = inputArray.length;
+return { average: average, min: min, max: max, length: length};
 }
 module.exports = analyzeArray;
